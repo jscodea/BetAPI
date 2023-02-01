4d81da14a8f30a2e0ff1336f6033ae7d38952c39
jscodea <jonas.siudikas@codeacademylt.onmicrosoft.com>
2023-02-01 18:47:49 +0200

    fix seeds

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -11,54 +11,52 @@ namespace BetAPI.Models
                 serviceProvider.GetRequiredService<
                     DbContextOptions<BetAPIContext>>()))
             {
-                if (!context.Event.Any())
+                if (context.Event.Any() || context.User.Any() || context.Bet.Any())
                 {
-                    context.Event.AddRange(
-                        new Event
-                        {
-                            Id = 1,
-                            Name = "Cat - Dog",
-                            Opt1 = 1.85m,
-                            Opt2 = 1.85m,
-                            BetsAllowedFrom = DateTime.Now,
-                            StartsAt = DateTime.Now.AddMinutes(5),
-                            EndsAt = DateTime.Now.AddMinutes(10),
-                        }
-                    );
+                    return;
                 }
-                if (!context.User.Any())
+
+                Event FirstEvent = new Event
                 {
-                    context.User.AddRange(
-                        new User
-                        {
-                            Id = 1,
-                            FirstName = "John",
-                            LastName = "Doe",
-                            IsActive = true,
-                            Balance = 999.99m,
-                            CreatedAt = DateTime.Now,
-                            UpdatedAt = DateTime.Now,
-                        }
-                    );
-                }
-                if (!context.Bet.Any())
+                    Name = "Cat - Dog",
+                    Opt1 = 1.85m,
+                    Opt2 = 1.85m,
+                    BetsAllowedFrom = DateTime.Now,
+                    StartsAt = DateTime.Now.AddMinutes(5),
+                    EndsAt = DateTime.Now.AddMinutes(10),
+                };
+                User FirstUser = new User
                 {
-                    context.Bet.AddRange(
-                        new Bet
-                        {
-                            Id = 1,
-                            UserId = 1,
-                            EventId = 1,
-                            IsCompleted = false,
-                            Opt = 2,
-                            Stake = 10.12m,
-                            Payout = 0,
-                            Odds = 1.85m,
-                            CreatedAt = DateTime.Now,
-                            UpdatedAt = DateTime.Now,
-                        }
-                    );
-                }
+                    FirstName = "John",
+                    LastName = "Doe",
+                    IsActive = true,
+                    Balance = 999.99m,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                };
+
+
+                context.Event.AddRange(
+                    FirstEvent
+                );
+                context.User.AddRange(
+                    FirstUser
+                );
+                context.SaveChanges();
+                context.Bet.AddRange(
+                    new Bet
+                    {
+                        UserId = FirstUser.Id,
+                        EventId = FirstEvent.Id,
+                        IsCompleted = false,
+                        Opt = 2,
+                        Stake = 10.12m,
+                        Payout = 0,
+                        Odds = 1.85m,
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now,
+                    }
+                );
                 context.SaveChanges();
             }
         }
